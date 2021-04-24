1a48ac33fa9eec8a336005b5d31e4721e7ee56b6
hamzamughal <mughalh473@gmail.com>
2021-04-24 14:16:59 +0500

    registration of user in database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-require('./server/db');
+require('./db');
 const userRouter = require('./routers/user');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
 app.use(express.json());
+app.use(cors())
 app.use(userRouter)
 
 app.get('/', (req, res) => {
